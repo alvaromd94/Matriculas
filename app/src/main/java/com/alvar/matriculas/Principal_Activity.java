3d62c51c6404a543a53a80aaf758fb969641d778
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2019-04-21 17:44:45 +0200

    Initial commit

---
+++ app/src/main/java/com/alvar/matriculas/Principal_Activity.java
@@ -0,0 +1,13 @@
+package com.alvar.matriculas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_);
+    }
+}
